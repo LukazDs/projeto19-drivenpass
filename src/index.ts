7721025ba3de473d91bc9b3e5c8b8cd25117500d
LukazDs <lukazds2016@gmail.com>
2022-09-09 15:41:48 -0300

    feat: create package of the initializing

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json(), cors());
+
+const PORT: number = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, () => { console.log(`Server running in the ${PORT}`) })
