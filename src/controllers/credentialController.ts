ece2143314e4b73a8a83cd9d3aea3cd6b417d6e5
LukazDs <lukazds2016@gmail.com>
2022-09-10 13:13:33 -0300

    feat: encrypting credential password by lib cryptr

--- src/controllers/credentialController.ts
+++ src/controllers/credentialController.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express";
 import * as credentialServices from "../services/credentialServices";
-import { ICredentials, ICredentialsBodyReq } from "../types/utilTypes";
+import { ICredentialsBodyReq } from "../types/utilTypes";
 
 export async function createCredential(req: Request, res: Response) {
 
     const credential: ICredentialsBodyReq = req.body;
     const { verified } = res.locals;
 
-    const payload: ICredentials = { ...credential, userId: verified.id };
+    const credentialUser = await credentialServices.findCredentialByUrl(credential, verified.id);
 
-    await credentialServices.findCredentialByUrl(payload)
-    await credentialServices.insertCredential(payload);
+    await credentialServices.insertCredential(credential, verified.id);
 
     res.status(201).send("Credential Created!");
 
--- src/services/credentialServices.ts
+++ src/services/credentialServices.ts
@@ -1,18 +1,29 @@
 import "express-async-errors";
 import dotenv from "dotenv";
+import cryptr from "cryptr";
 import { ICredentials, ICredentialsBodyReq } from "../types/utilTypes";
 import * as credentialRepository from "../repositories/credentialRepository";
 
 dotenv.config();
 
-export async function insertCredential(credential: ICredentials) {
+export async function insertCredential(credential: ICredentialsBodyReq, userId: number) {
 
-    await credentialRepository.insertCredential(credential);
+    const encryptCryptr = new cryptr(String(process.env.CRYPTR_KEY));
+
+    const encryptedPassword: string = encryptCryptr.encrypt(credential.password)
+
+    credential.password = encryptedPassword;
+
+    const payload: ICredentials = { ...credential, userId };
+
+    await credentialRepository.insertCredential(payload);
 
 }
 
-export async function findCredentialByUrl(credential: ICredentials) {
+export async function findCredentialByUrl(credential: ICredentialsBodyReq, userId: number) {
 
-    return await credentialRepository.findCredential(credential);
+    const payload: ICredentials = { ...credential, userId };
+
+    return await credentialRepository.findCredential(payload);
 
 }
