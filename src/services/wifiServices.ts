921fd56ab5c5ecf44f1ee21749f8497e003ee700
LukazDs <lukazds2016@gmail.com>
2022-09-12 15:12:53 -0300

    feat: encrypt password

--- src/services/wifiServices.ts
+++ src/services/wifiServices.ts
@@ -1,15 +1,23 @@
 import { IWifi, IWifiBodyReq } from "../types/utilTypes";
 import * as wifiRepository from "../repositories/wifiRepository";
+import cryptr from "cryptr";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function encryptPassword(wifi: IWifi) {
+
+    const encryptCryptr = new cryptr(String(process.env.CRYPTR_KEY));
+    const encryptedPassword: string = encryptCryptr.encrypt(wifi.password)
+
+    wifi.password = encryptedPassword;
+}
 
 export async function insertWifi(wifi: IWifiBodyReq, userId: number) {
 
     const payload: IWifi = { ...wifi, userId };
 
-    const wifis = await wifiRepository.findWifi(payload);
-
-    if (wifis.length) {
-        throw { code: "Conflict", message: "Wifi já cadastrado!" };
-    }
+    await encryptPassword(payload);
 
     await wifiRepository.insertWifi(payload);
 
